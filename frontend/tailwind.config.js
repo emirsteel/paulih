620d4d8b8ae527407f2437d1b32465fea8a99b8d
Emir Çelik <107912411+emirsteel@users.noreply.github.com>
2025-04-30 21:06:28 +0300

    Add files via upload

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}" ,  // Ensure the content paths are correct
+    ],
+    
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  

d8e1dcabf6806fb5fa85a4b5fbc601731d7882d1
Emir Çelik <107912411+emirsteel@users.noreply.github.com>
2025-04-30 22:47:32 +0300

    Add files via upload

---
+++ frontend/src/utils/cropImage.js
@@ -0,0 +1,43 @@
+import Cropper from "cropperjs";
+
+const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", (error) => reject(error));
+    image.setAttribute("crossOrigin", "anonymous"); // Needed to avoid CORS issues
+    image.src = url;
+  });
+
+export default async function getCroppedImg(imageSrc, crop) {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = crop.width;
+  canvas.height = crop.height;
+
+  ctx.drawImage(
+    image,
+    crop.x,
+    crop.y,
+    crop.width,
+    crop.height,
+    0,
+    0,
+    crop.width,
+    crop.height
+  );
+
+  return new Promise((resolve, reject) => {
+    canvas.toBlob((blob) => {
+      if (!blob) {
+        reject("Canvas is empty");
+        return;
+      }
+      const file = new File([blob], "cropped-image.jpeg", { type: "image/jpeg" });
+      const url = URL.createObjectURL(file);
+      resolve(url);
+    }, "image/jpeg");
+  });
+}

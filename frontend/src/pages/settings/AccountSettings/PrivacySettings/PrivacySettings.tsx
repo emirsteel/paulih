7dbc7e34f232c041471af7b70abd02fdc887aa8f
Emir Çelik <107912411+emirsteel@users.noreply.github.com>
2025-04-30 22:38:43 +0300

    Add files via upload

---
+++ frontend/src/pages/settings/AccountSettings/PrivacySettings/PrivacySettings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PrivacySettings: React.FC = () => {
+  return (
+    <div>
+      <h2 className="text-lg font-bold text-gray-600 mb-4">Privacy Settings</h2>
+      <p>Configure your privacy preferences here.</p>
+    </div>
+  );
+};
+
+export default PrivacySettings;

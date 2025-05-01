59f6eadbe21f1a201463892c0e311a9c736c5111
Emir Çelik <107912411+emirsteel@users.noreply.github.com>
2025-05-01 11:35:14 +0300

    Delete backend/dist directory

--- backend/dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// user.routes.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const router = (0, express_1.Router)();
-// Signup Route
-router.post('/signup', user_controller_1.signup);
-exports.default = router;
